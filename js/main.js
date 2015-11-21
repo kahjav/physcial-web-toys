d7541e2eb7656156301bb376e39f3452ae7059bd
Kevin <kevinahuber@gmail.com>
2015-11-20 23:03:21 -0600

    Vibrate

--- js/main.js
+++ js/main.js
@@ -2,32 +2,16 @@
   Return a random element from the passed in array.
   http://stackoverflow.com/questions/4550505/getting-random-value-from-an-array
 */
-if(window.navigator) {
-  var vibrate = window.navigator.vibrate
-}
 var batteryDisplay = document.getElementById('battery')
 
 navigator.getBattery().then(function (battery) {
-   batteryDisplay.innerHTML = "Battery: " + battery.level + (battery.charging ? "(charging)" : "")
-
-   battery.addEventListener('chargingchange', function() {
-    batteryDisplay.innerHTML = "Battery charging? " + (battery.charging ? "Yes" : "No")
-  });
-
-  battery.addEventListener('levelchange', function() {
-    batteryDisplay.innerHTML = "Battery level: " + battery.level * 100 + "%"
-  });
-
-  battery.addEventListener('chargingtimechange', function() {
-    batteryDisplay.innerHTML =  "Battery charging time: " + battery.chargingTime + " seconds"
-  });
-
-  battery.addEventListener('dischargingtimechange', function() {
-    batteryDisplay.innerHTML = "Battery discharging time: " + battery.dischargingTime + " seconds"
-  });
+  battery.addEventListener('chargingchange', displayBattery(battery));
+  battery.addEventListener('levelchange', displayBattery(battery));
 }, function () {
   batteryDisplay.innerHTML = 'Battery API not Present'
 })
+
+navigator.vibrate(Infinity)
 
 if ('Notification' in window) {
   var notification = new Notification('Ah!', {
@@ -42,6 +26,8 @@ if ('ondeviceproximity' in window) {
   });
 }
 
-if (vibrate) {
-  navigator.vibrate(3000)
+
+
+function displayBattery(battery) {
+  batteryDisplay.innerHTML = "Battery: " + battery.level * 100 + "%" + (battery.charging ? "(charging)" : "")
 }
